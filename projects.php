d9b7c5fbfdc53fe8c5ee8300b4072b5184c625bf
TemaSaur <TemaSaur@gmail.com>
2020-08-14 17:18:59 +0500

    added htaccess file
    fixed file extension in url via .htaccess file

--- projects.php
+++ projects.php
@@ -12,7 +12,7 @@
 					Well, actually, the portfolio is missing at the moment but, it’s just due to the fact, that I don’t find it interesting to post all my study projects online. But be sure, in a short time there will be something interesting around here.
 				</p>
 				<p>
-					Maybe with <a href="contact.php" class="link">your help</a> I'll be able to put something new to this page!
+					Maybe with <a href="contact" class="link">your help</a> I'll be able to put something new to this page!
 				</p>
 			</div>
 		</div>
