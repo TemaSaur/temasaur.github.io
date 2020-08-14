4e1980a99b9fa0669df63be64fbc614a47c0df2c
TemaSaur <TemaSaur@gmail.com>
2020-08-14 17:11:56 +0500

    added Russian language

---
+++ ru/includes/der.php
@@ -0,0 +1,8 @@
+	</title>
+	<script src="../js/cursor.js"></script>
+	<script src="../js/planet.js"></script>
+	<script src="../js/stars.js"></script>
+	<script src="../js/app.js"></script>
+</head>
+<body>
+	<span id="cursor"></span>
