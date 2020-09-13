628eb96dc694c7aec40375d9cba19fd668c683dc
TemaSaur <TemaSaur@gmail.com>
2020-09-13 10:56:03 +0500

    minified stuff

--- ru/includes/der.php
+++ ru/includes/der.php
@@ -1,8 +1,8 @@
 	</title>
-	<script src="../js/cursor.js"></script>
-	<script src="../js/planet.js"></script>
-	<script src="../js/stars.js"></script>
-	<script src="../js/app.js"></script>
+	<script src="../js/cursor.min.js"></script>
+	<script src="../js/planet.min.js"></script>
+	<script src="../js/stars.min.js"></script>
+	<script src="../js/app.min.js"></script>
 </head>
 <body>
 	<span id="cursor"></span>
