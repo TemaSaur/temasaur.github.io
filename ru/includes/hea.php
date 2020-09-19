38e98897402e35b844ba7af91a8fa1a0ccfb22c8
TemaSaur <TemaSaur@gmail.com>
2020-09-19 20:06:47 +0500

    removed main.min.css

--- ru/includes/hea.php
+++ ru/includes/hea.php
@@ -4,5 +4,5 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Bad+Script&family=Gabriela&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="../css/main.min.css">
+	<link rel="stylesheet" href="../css/main.css">
 	<title>
